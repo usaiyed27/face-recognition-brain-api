740a7184b819f25bf87ae008ab050a945627c5e3
Urusa <usaiyed.9127@gmail.com>
2021-02-19 18:19:46 -0700

    added signin and register route

--- server.js
+++ server.js
@@ -1,11 +1,68 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
-app.get('/', (req, res) => {
-	res.send('helloo');
+app.use(bodyParser.json());
+const database = {
+	users: [
+		{
+			id: 123,
+			name: 'john',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: 124,
+			name: 'sara',
+			email: 'sara@email.com',
+			password: 'candies',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+app.get('/',(req,res) => {
+	res.send('This is working..')
 })
-app.listen(3000);
+
+app.post('/signin', (req,res) => {
+	if(req.body.email === database.users[0].email && 
+	   req.body.password === database.users[0].password){
+		res.json('success');
+	}else{
+		res.json('Error logging in..')
+	}
+})
+
+app.post('/register', (req,res) => {
+	const { email, name, password} = req.body;
+	database.users.push({
+		id: 124,
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length -1])
+})
+
+app.listen(3000, () => {
+	console.log('App is running on port 3000');
+})
+
+/*
+	/ --> res = this is working
+	/signin --> POST = success/fail
+	/register --> POST = user
+	/profile/:userId --> GET = user
+	/image --> PUT = user score
+*/
+
+
+
 // const express = ;require('express');
 // const bodyParser = require('body-parser');
 // const bcrypt = require('bcrypt-nodejs');
