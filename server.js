148b814acbc14465ffeca39bbd6e7bbd20024c96
Urusa <usaiyed.9127@gmail.com>
2021-02-21 11:54:10 -0700

    connected server to database

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  	client: 'pg',
+  	connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'firdaus',
+    database : 'smart_brain'
+  }
+});
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+});
 
 const app = express();
 
@@ -77,7 +92,7 @@ app.get('/profile/:id', (req,res) => {
 	}
 })
 
-app.post('/image', (req,res) => {
+app.put('/image', (req,res) => {
 	const { id } = req.body;
 	let found = false;
 	database.users.forEach(user => {
