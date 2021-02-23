59a82940d9087c404ca058073dc5e4baa28f233f
Urusa <usaiyed.9127@gmail.com>
2021-02-22 17:05:56 -0700

    added imageurl route

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ 		apiKey: '5e048cb9286845cfbc0dff14238b93fc'
+});
+
+const handleApiCall = (req, res) => {
+app.models
+	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data => { 
+		res.json(data)
+	})
+	.catch(err => res.status(400).json('unable to work with api'))
+}
+	
+
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
 	db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-	handleImage: handleImage
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
 }
