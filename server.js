1e845169303917987968bc8ee901f288dbd478b7
Urusa <usaiyed.9127@gmail.com>
2021-03-04 20:57:07 -0700

    connecting to database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   	client: 'pg',
   	connection: {
-    host : 'postgresql-opaque-55329',
-    user : 'postgres',
-    password : 'firdaus',
-    database : 'smart_brain'
+    host : 'process.env.DATABASE_URL',
+    ssl: true
   }
 });
 
