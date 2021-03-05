7c48d6fc4c54d56214dea3cd1594d78d9f76007f
Urusa <usaiyed.9127@gmail.com>
2021-03-04 20:52:00 -0700

    updatating connection

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   	client: 'pg',
   	connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-opaque-55329',
     user : 'postgres',
     password : 'firdaus',
     database : 'smart_brain'
